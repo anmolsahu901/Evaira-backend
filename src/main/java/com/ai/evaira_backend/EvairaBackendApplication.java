c6c472a279fa2bb8029549a7e9a2b38a6aa0d28f
anmolsahu901 <anmolsahu901@gmail.com>
2025-12-07 16:11:44 +0530

    Initial commit

---
+++ src/main/java/com/ai/evaira_backend/EvairaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ai.evaira_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvairaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvairaBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ai/evaira_backend/EvairaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ai.evaira_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvairaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
